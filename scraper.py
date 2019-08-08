bd4ff6bc8634ffc6a9ce340646a920216a73a5d3
lexie11 <44230267+lexie11@users.noreply.github.com>
2019-08-08 09:05:44 +0100

    changed str(&rows=100) as invalid syntax
    to '&rows=100'

--- scraper.py
+++ scraper.py
@@ -43,6 +43,6 @@ def scrape(parameter_b):
 starting_url = 'https://reports.ofsted.gov.uk/search?q=&location=&radius=&level_2_types%5B0%5D=2&region%5B0%5D=E12000007&status%5B0%5D=1&level_1_types=1&start='
 i = 0
 while i < 800:
-    url = starting_url + str(i) + str(&rows=100)
+    url = starting_url + str(i) + '&rows=100'
     scrape(url)
     i += 100
